40bedb744a833fd7e0f3bb68d9e9e395ef533117
Tomas <tomas.engebretsen@gmail.com>
2023-05-04 21:45:00 +0200

    Work on UnitInput

--- src/components/UnitInput/UnitInput.stories.ts
+++ src/components/UnitInput/UnitInput.stories.ts
@@ -1,5 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { UnitInput } from './UnitInput';
+import type {Meta, StoryObj} from '@storybook/react';
+import {UnitInput} from './UnitInput';
+import {Unit, UnitName} from 'enheter';
 
 const meta = {
   title: 'Components/UnitInput',
@@ -16,5 +17,32 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Example: Story = {
-  args: {},
+  args: {
+    dimension: 'length',
+    textFn: (unit: Unit) => {
+      //@formatter:off
+      switch (unit.key as UnitName<'length'>) {
+        case 'metre': return 'Metres';
+        case 'foot': return 'Feet';
+        case 'inch': return 'Inches';
+        case 'yard': return 'Yards';
+        case 'nauticalMile': return 'Nautical miles';
+        case 'statuteMile': return 'Statute miles';
+        case 'astronomicalUnit': return 'Astronomical units';
+        case 'lightYear': return 'Light years';
+        case 'parsec': return 'Parsecs';
+        case 'angstrom': return 'Ångstrøms';
+        case 'fermi': return 'Fermis';
+        case 'micron': return 'Microns';
+        case 'chain': return 'Chains';
+        case 'fathom': return 'Fathoms';
+        case 'furlong': return 'Furlongs';
+        case 'link': return 'Links';
+        case 'rod': return 'Rods';
+        case 'scandinavianMile': return 'Scandinavian miles';
+        default: return unit.key || '';
+      }
+      //@formatter:on
+    },
+  },
 };
--- src/components/UnitInput/UnitInput.tsx
+++ src/components/UnitInput/UnitInput.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import {Select, SingleSelectOption, TextField} from '@digdir/design-system-react';
+import type {DimensionName} from 'enheter';
+import {allUnits, Unit} from 'enheter';
+import '@altinn/figma-design-tokens/dist/tokens.css';
 
-export const UnitInput = () => {
-  return <input type='number'/>;
+export interface UnitInputProps<T extends DimensionName> {
+  dimension: T;
+  textFn: (unit: Unit) => string;
+}
+
+export const UnitInput = <T extends DimensionName>({dimension, textFn}: UnitInputProps<T>) => {
+  const {units} = allUnits[dimension];
+  const options: SingleSelectOption[] = Object.values(units).map((unit) => ({
+    label: textFn(unit),
+    value: unit.key,
+  }));
+  return <>
+    <TextField
+      formatting={{number: {}}}
+    />
+    <Select options={options}/>
+  </>;
 }
