29d8208e5ef0d30cec323e31c6554514abb1b2f5
Tomas <tomas.engebretsen@gmail.com>
2023-05-06 23:07:58 +0200

    Further work on UnitInput

--- src/components/UnitInput/UnitInput.tsx
+++ src/components/UnitInput/UnitInput.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Select, SingleSelectOption, TextField} from '@digdir/design-system-react';
 import type {DimensionName} from 'enheter';
 import {allUnits, Unit} from 'enheter';
 import '@altinn/figma-design-tokens/dist/tokens.css';
+import style from './UnitInput.module.css';
 
 export interface UnitInputProps<T extends DimensionName> {
   dimension: T;
@@ -11,14 +12,29 @@ export interface UnitInputProps<T extends DimensionName> {
 
 export const UnitInput = <T extends DimensionName>({dimension, textFn}: UnitInputProps<T>) => {
   const {units} = allUnits[dimension];
+  const [value, setValue] = React.useState<number>(1);
+  const [unit, setUnit] = React.useState<string>(Object.values(units)[0].baseUnit.key);
+  useEffect(() => {
+    setUnit(Object.values(units)[0].baseUnit.key);
+  }, [dimension, units]);
   const options: SingleSelectOption[] = Object.values(units).map((unit) => ({
     label: textFn(unit),
     value: unit.key,
   }));
-  return <>
-    <TextField
-      formatting={{number: {}}}
-    />
-    <Select options={options}/>
-  </>;
+  return <span className={style.unitInput}>
+    <span>
+      <TextField
+        formatting={{number: {}, align: 'right'}}
+        onChange={(e) => setValue(Number(e.target.value))}
+        value={value.toString()}
+      />
+    </span>
+    <span>
+      <Select
+        options={options}
+        onChange={setUnit}
+        value={unit}
+      />
+    </span>
+  </span>;
 }
